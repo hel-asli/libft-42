eb3293cc6707a27146991d6495a7f03b81309314
hel-asli <helasli528@gmail.com>
2023-11-09 23:51:54 +0100

    ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -5,72 +5,71 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hel-asli <hel-asli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/09 15:39:53 by hel-asli          #+#    #+#             */
-/*   Updated: 2023/11/09 16:41:42 by hel-asli         ###   ########.fr       */
+/*   Created: 2023/11/09 22:25:06 by hel-asli          #+#    #+#             */
+/*   Updated: 2023/11/09 22:44:30 by hel-asli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
-static int front_set_count(char const *s1, char const *set)
+int ft_start(const char *s1, const char *set)
 {
-	size_t set_len;
+	size_t len;
 	size_t i;
 
+	len = ft_strlen(s1);
 	i = 0;
-	set_len = ft_strlen(set);
-	while (i < set_len)
+	
+	while (i < len)
 	{
-		if (s1[i] != set[i])
-			return 0;
+		if (ft_strchr(set, s1[i]) == 0)
+			break;
 		i++;
 	}
-	return 1;
+	return i;
 }
-static int back_set_count(char const *s1, char const *set)
+
+int ft_end(const char *s1, const char *set)
 {
-	
-	size_t set_len;
+	size_t len;
 	size_t i;
 
-	i = ft_strlen(s1);
-	set_len = ft_strlen(set);
-	while (set_len > 0)
+	len = ft_strlen(s1);
+	i = 0;
+	while (i < len)
 	{
-		if (s1[i - 1] != set[set_len - 1])
-			return 0;
-		i--;
-		set_len--;
+		if (ft_strchr(set , s1[len - i -1]) == 0)
+			break;
+		i++;
 	}
-	return 1;
+	return len - i;
 }
 
 char *ft_strtrim(char const *s1, char const *set)
 {
-	if (!s1 || !set)
-		return NULL;
-	size_t s1_len = ft_strlen(s1);
-	size_t set_len = ft_strlen(set);
-	unsigned int size;
-	int i;
+	int	start;
+	int end;
 	char *str;
-	int a = front_set_count(s1, set);
-	int b = back_set_count(s1, set);
 
-	size = 0;
-	if (a && b)
-	{
-		size = s1_len - (2 * set_len);
-		if (size < 0)
-			size = 0;
-	}
+	if (!s1)
+		return NULL;
+	if (!set)
+		return ft_strdup(s1);
+	start = ft_start(s1, set);
+	end = ft_end(s1, set);
+	if (start >= end)
+		return ft_strdup("");
+	str = (char *)malloc(sizeof(char) * (end - start + 1));
 
-	str = (char *)malloc(size * sizeof(char));
 	if (!str)
 		return NULL;
-	i = 0;	
-	while(i < size)	
-		str[i++] = s1[set_len++];
-	str[i] = '\0';
-
-	return str;	
+	ft_strlcpy(str, s1+start, end - start + 1);
+	return str;
 }
+/*
+int main (void)
+{
+	char *str = "ab*bb kksjfal f;askdjf*ba";
+	printf("%s\n", ft_strtrim(str, "ab"));
+}
+*/
