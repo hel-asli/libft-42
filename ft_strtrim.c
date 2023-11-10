d031ae9bcec73c9aa82b468c09a85cefd9b7aad2
hel-asli <helasli528@gmail.com>
2023-11-10 14:31:03 +0100

    solution 2 ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,37 +12,47 @@
 
 #include "libft.h"
 
-int ft_start(const char *s1, const char *set)
+static size_t ft_getstart(char const *s1, char const *set)
 {
-	size_t len;
 	size_t i;
-
-	len = ft_strlen(s1);
-	i = 0;
+	size_t j;
 	
-	while (i < len)
+	i = 0;
+	while(s1[i] != '\0')
 	{
-		if (ft_strchr(set, s1[i]) == 0)
+		j = 0;
+		while (set[j] != '\0')
+		{
+			if (s1[i] == set[j])
+				break;
+			j++;
+		}
+		if (set[j] == '\0')
 			break;
 		i++;
 	}
 	return i;
 }
-
-int ft_end(const char *s1, const char *set)
+static size_t ft_getend(char const *s1, char const *set)
 {
-	size_t len;
 	size_t i;
+	size_t j;
 
-	len = ft_strlen(s1);
-	i = 0;
-	while (i < len)
+	i = ft_strlen(s1);
+ 	while(i >= 0)
 	{
-		if (ft_strchr(set , s1[len - i -1]) == 0)
+		j = 0;
+		while (set[j])
+		{
+			if (s1[i - 1] == set[j])
+				break;
+			j++;
+		}
+		if(set[j] == '\0')
 			break;
-		i++;
+		i--;
 	}
-	return len - i;
+	return i;
 }
 
 char *ft_strtrim(char const *s1, char const *set)
@@ -55,8 +65,8 @@ char *ft_strtrim(char const *s1, char const *set)
 		return NULL;
 	if (!set)
 		return ft_strdup(s1);
-	start = ft_start(s1, set);
-	end = ft_end(s1, set);
+	start = ft_getstart(s1, set);
+	end = ft_getend(s1, set);
 	if (start >= end)
 		return ft_strdup("");
 	str = (char *)malloc(sizeof(char) * (end - start + 1));
@@ -66,10 +76,3 @@ char *ft_strtrim(char const *s1, char const *set)
 	ft_strlcpy(str, s1+start, end - start + 1);
 	return str;
 }
-/*
-int main (void)
-{
-	char *str = "ab*bb kksjfal f;askdjf*ba";
-	printf("%s\n", ft_strtrim(str, "ab"));
-}
-*/
